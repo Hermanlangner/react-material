2ec6ef2042962396b47766279e40e71f928987e6
Herman Langner <hermandlangner@gmail.com>
2018-10-24 23:04:49 +0200

    pages

---
+++ build/precache-manifest.f9466935a7a137a9f0cd645067e56c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "352f12e4efb3a0f67838",
+    "url": "/react-material/static/css/main.45751397.chunk.css"
+  },
+  {
+    "revision": "352f12e4efb3a0f67838",
+    "url": "/react-material/static/js/main.352f12e4.chunk.js"
+  },
+  {
+    "revision": "0f2db7bb480447cd682d",
+    "url": "/react-material/static/js/1.0f2db7bb.chunk.js"
+  },
+  {
+    "revision": "116f1837e6febd3a8a61",
+    "url": "/react-material/static/js/runtime~main.116f1837.js"
+  },
+  {
+    "revision": "b235d56d1ba775c9cd22ce795ee094b2",
+    "url": "/react-material/index.html"
+  }
+];
